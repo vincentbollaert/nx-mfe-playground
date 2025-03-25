84e8f9d1617cae69caae6405d0e578c82c9676ce
Vincent Bollaert <vincentbollaert@gmail.com>
2025-03-25 23:10:39 +0100

    add nx

--- apps/client-react/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import { mergeViteConfig } from "@repo/vitest-config/configs";
-export default defineConfig(() => mergeViteConfig({
-    test: {
-        setupFiles: ["./setupTests.ts"],
-    },
-}));
--- apps/client-react/vite.config.ts
+++ apps/client-react/vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "vite";
-import { mergeViteConfig } from "@repo/vitest-config/configs";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
 
-export default defineConfig(() =>
-  mergeViteConfig({
-    test: {
-      setupFiles: ["./setupTests.ts"],
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./setupTests.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.{ts,tsx}"],
     },
-  }),
-);
+  },
+});
+
+// vite was unable to resolve this config saved with ts extension on a new machine. look into this
+// import { mergeViteConfig } from "@repo/vitest-config/configs";
+
+// export default defineConfig(() =>
+//   mergeViteConfig({
+//     test: {
+//       setupFiles: ["./setupTests.ts"],
+//     },
+//   }),
+// );
