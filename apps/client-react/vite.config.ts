ab4ea6adda09e3c4e200c4352b951ab91478cded
Vincent Bollaert <vincentbollaert@gmail.com>
2025-03-26 08:25:37 +0100

    fix vite not recognising ts vite configs

--- apps/client-react/vite.config.ts
+++ apps/client-react/vite.config.ts
@@ -1,27 +1,10 @@
-import react from "@vitejs/plugin-react-swc";
+import { mergeViteConfig } from "@repo/vitest-config/configs";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["./setupTests.ts"],
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-      include: ["src/**/*.{ts,tsx}"],
+export default defineConfig(() =>
+  mergeViteConfig({
+    test: {
+      setupFiles: ["./setupTests.ts"],
     },
-  },
-});
-
-// vite was unable to resolve this config saved with ts extension on a new machine. look into this
-// import { mergeViteConfig } from "@repo/vitest-config/configs";
-
-// export default defineConfig(() =>
-//   mergeViteConfig({
-//     test: {
-//       setupFiles: ["./setupTests.ts"],
-//     },
-//   }),
-// );
+  }),
+);
